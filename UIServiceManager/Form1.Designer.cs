a1adf4ff3b248bc764030f1e8d19fc79bd3f56f4
Mircea Varvaroi <mircea.varvaroi@student.usv.ro>
2023-05-24 02:28:47 +0300

    Add search

--- UIServiceManager/Form1.Designer.cs
+++ UIServiceManager/Form1.Designer.cs
@@ -34,11 +34,27 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(513, 25);
+            this.textBox2.Location = new System.Drawing.Point(166, 120);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(123, 20);
             this.textBox2.TabIndex = 1;
@@ -46,7 +62,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(128, 25);
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(121, 20);
             this.textBox1.TabIndex = 2;
@@ -54,7 +70,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(255, 25);
+            this.textBox3.Location = new System.Drawing.Point(166, 57);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(124, 20);
             this.textBox3.TabIndex = 3;
@@ -62,7 +78,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(385, 25);
+            this.textBox4.Location = new System.Drawing.Point(12, 120);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(122, 20);
             this.textBox4.TabIndex = 4;
@@ -70,7 +86,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(655, 25);
+            this.button1.Location = new System.Drawing.Point(12, 233);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 26);
             this.button1.TabIndex = 5;
@@ -80,7 +96,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(655, 57);
+            this.button2.Location = new System.Drawing.Point(12, 486);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(119, 26);
             this.button2.TabIndex = 6;
@@ -88,11 +104,171 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleGreen;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Client Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.PaleGreen;
+            this.label2.Location = new System.Drawing.Point(168, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Car Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.PaleGreen;
+            this.label4.Location = new System.Drawing.Point(168, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Client Surname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleGreen;
+            this.label5.Location = new System.Drawing.Point(12, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Car Mark";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(13, 317);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.PaleGreen;
+            this.label6.Location = new System.Drawing.Point(13, 301);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Client Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.PaleGreen;
+            this.label7.Location = new System.Drawing.Point(13, 353);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Car Mark";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(13, 369);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(122, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(166, 317);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(124, 20);
+            this.textBox7.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.PaleGreen;
+            this.label8.Location = new System.Drawing.Point(163, 301);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Client Surname";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.PaleGreen;
+            this.label9.Location = new System.Drawing.Point(163, 353);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Car Model";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(166, 369);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(123, 20);
+            this.textBox8.TabIndex = 19;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 426);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 26);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Search Car";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(12, 185);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(121, 20);
+            this.textBox9.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.PaleGreen;
+            this.label10.Location = new System.Drawing.Point(9, 169);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Car Odometer";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 602);
+            this.ClientSize = new System.Drawing.Size(367, 542);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox4);
@@ -114,6 +290,22 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- UIServiceManager/Form1.cs
+++ UIServiceManager/Form1.cs
@@ -12,6 +12,7 @@ using System.Configuration;
 using ModelLib;
 using DataAccessCar;
 using DataAccessClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 namespace UIServiceManager
@@ -27,7 +28,7 @@ namespace UIServiceManager
             InitializeComponent();
 
             //setare proprietati
-            this.Size = new Size(900, 700);
+            this.Size = new Size(330, 700);
             this.StartPosition = FormStartPosition.Manual;
             this.Location = new Point(400, 400);
             this.Font = new Font("Arial", 9, FontStyle.Bold);
@@ -48,6 +49,7 @@ namespace UIServiceManager
             lblId = new Label();
             lblId.Width = LATIME_CONTROL;
             lblId.Text = "Id";
+            lblId.Left = 3 * DIMENSIUNE_PAS_X;
             lblId.BackColor = Color.LightYellow;
             this.Controls.Add(lblId);
 
@@ -55,28 +57,28 @@ namespace UIServiceManager
             lblName = new Label();
             lblName.Width = LATIME_CONTROL;
             lblName.Text = "Name";
-            lblName.Left = DIMENSIUNE_PAS_X;
+            lblName.Left = 4 * DIMENSIUNE_PAS_X;
             lblName.BackColor = Color.LightYellow;
             this.Controls.Add(lblName);
 
             lblSurname = new Label();
             lblSurname.Width = LATIME_CONTROL;
             lblSurname.Text = "Surname";
-            lblSurname.Left = 2 * DIMENSIUNE_PAS_X;
+            lblSurname.Left = 5 * DIMENSIUNE_PAS_X;
             lblSurname.BackColor = Color.LightYellow;
             this.Controls.Add(lblSurname);
 
             lblMark = new Label();
             lblMark.Width = LATIME_CONTROL;
             lblMark.Text = "Car Mark";
-            lblMark.Left = 3 * DIMENSIUNE_PAS_X;
+            lblMark.Left = 6 * DIMENSIUNE_PAS_X;
             lblMark.BackColor = Color.LightYellow;
             this.Controls.Add(lblMark);
 
             lblModel = new Label();
             lblModel.Width = LATIME_CONTROL;
             lblModel.Text = "Car Model";
-            lblModel.Left = 4 * DIMENSIUNE_PAS_X;
+            lblModel.Left = 7 * DIMENSIUNE_PAS_X;
             lblModel.BackColor = Color.LightYellow;
             this.Controls.Add(lblModel);
         }
@@ -94,6 +96,7 @@ namespace UIServiceManager
                 lblIds[i] = new Label();
                 lblIds[i].Width = LATIME_CONTROL;
                 lblIds[i].Text = clients[i].idClient.ToString();
+                lblIds[i].Left = 3 * DIMENSIUNE_PAS_X;
                 lblIds[i].Top = (i + 2) * DIMENSIUNE_PAS_Y;
                 lblIds[i].BackColor = Color.WhiteSmoke;
                 this.Controls.Add(lblIds[i]);
@@ -101,7 +104,7 @@ namespace UIServiceManager
                 lblNames[i] = new Label();
                 lblNames[i].Width = LATIME_CONTROL;
                 lblNames[i].Text = clients[i].name;
-                lblNames[i].Left = DIMENSIUNE_PAS_X;
+                lblNames[i].Left = 4 * DIMENSIUNE_PAS_X;
                 lblNames[i].Top = (i + 2) * DIMENSIUNE_PAS_Y;
                 lblNames[i].BackColor = Color.WhiteSmoke;
                 this.Controls.Add(lblNames[i]);
@@ -109,7 +112,7 @@ namespace UIServiceManager
                 lblSurnames[i] = new Label();
                 lblSurnames[i].Width = LATIME_CONTROL;
                 lblSurnames[i].Text = clients[i].surname;
-                lblSurnames[i].Left = 2 * DIMENSIUNE_PAS_X;
+                lblSurnames[i].Left = 5 * DIMENSIUNE_PAS_X;
                 lblSurnames[i].Top = (i + 2) * DIMENSIUNE_PAS_Y;
                 lblSurnames[i].BackColor = Color.WhiteSmoke;
                 this.Controls.Add(lblSurnames[i]);
@@ -117,7 +120,7 @@ namespace UIServiceManager
                 lblMarks[i] = new Label();
                 lblMarks[i].Width = LATIME_CONTROL;
                 lblMarks[i].Text = cars[i].mark;
-                lblMarks[i].Left = 3 * DIMENSIUNE_PAS_X;
+                lblMarks[i].Left = 6 * DIMENSIUNE_PAS_X;
                 lblMarks[i].Top = (i + 2) * DIMENSIUNE_PAS_Y;
                 lblMarks[i].BackColor = Color.WhiteSmoke;
                 this.Controls.Add(lblMarks[i]);
@@ -125,16 +128,11 @@ namespace UIServiceManager
                 lblModels[i] = new Label();
                 lblModels[i].Width = LATIME_CONTROL;
                 lblModels[i].Text = cars[i].model;
-                lblModels[i].Left = 4 * DIMENSIUNE_PAS_X;
+                lblModels[i].Left = 7 * DIMENSIUNE_PAS_X;
                 lblModels[i].Top = (i + 2) * DIMENSIUNE_PAS_Y;
                 lblModels[i].BackColor = Color.WhiteSmoke;
                 this.Controls.Add(lblModels[i]);
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -157,6 +155,40 @@ namespace UIServiceManager
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string carFileName = ConfigurationManager.AppSettings["CarFile"];
+            string clientFileName = ConfigurationManager.AppSettings["ClientFile"];
+            string localSolutionFile = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string clientFileFullPath = localSolutionFile + "\\" + clientFileName;
+            string carFileFullPath = localSolutionFile + "\\" + carFileName;
+            AdminClient adminClient = new AdminClient(clientFileFullPath);
+            AdminCar adminCar = new AdminCar(carFileFullPath);
+            Car car = new Car();
+            Client client = new Client();
+            int entryNb = 0;
+            adminCar.GetCars(out entryNb);
+            int Id = entryNb + 1;
+
+            car = new Car(Id, textBox4.Text, textBox2.Text, textBox9.Text);
+            client = new Client(Id, textBox1.Text, textBox3.Text);
+
+            entryNb++;
+            adminCar.AddCar(car);
+            adminClient.AddClient(client);
+
+            textBox4.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox1.Text = string.Empty;
+            textBox9.Text = string.Empty;
+
+            Client[] clients = adminClient.GetClients(out entryNb);
+            Car[] cars = adminCar.GetCars(out entryNb);
+            FormGetAndShowInfo(clients, cars, entryNb);
+            this.Width = 1200;
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
             string carFileName = ConfigurationManager.AppSettings["CarFile"];
@@ -171,6 +203,114 @@ namespace UIServiceManager
             Car[] cars = adminCar.GetCars(out entryNb);
 
             FormGetAndShowInfo(clients, cars, entryNb);
+            this.Width = 1200;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Id3 = 0;
+            string mark3 = textBox6.Text;
+            string model3 = textBox8.Text;
+            string name3 = textBox5.Text;
+            string surname3 = textBox7.Text;
+            Client client3 = new Client(Id3, name3, surname3);
+            Car car3 = new Car();
+            bool entryFound = false;
+            string carFileName = ConfigurationManager.AppSettings["CarFile"];
+            string clientFileName = ConfigurationManager.AppSettings["ClientFile"];
+            string localSolutionFile = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string clientFileFullPath = localSolutionFile + "\\" + clientFileName;
+            string carFileFullPath = localSolutionFile + "\\" + carFileName;
+            AdminClient adminClient = new AdminClient(clientFileFullPath);
+            AdminCar adminCar = new AdminCar(carFileFullPath);
+
+            Int32.TryParse(Console.ReadLine(), out Id3);
+            if (Id3 == 0)
+            {
+                if (string.IsNullOrEmpty(mark3))
+                {
+                    if (string.IsNullOrEmpty(model3))
+                    {
+                        if (string.IsNullOrEmpty(name3))
+                        {
+                            if (string.IsNullOrEmpty(surname3))
+                            {
+                                label6.BackColor = Color.Red;
+                                label8.BackColor = Color.Red;
+                                label7.BackColor = Color.Red;
+                                label9.BackColor = Color.Red;
+                            }
+                            else
+                            {
+                                client3 = adminClient.GetClient(name3, surname3, Id3);
+                                if (string.Equals(surname3, client3.surname))
+                                {
+                                    car3 = adminCar.GetCar(mark3, model3, client3.idClient);
+                                    entryFound = true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            client3 = adminClient.GetClient(name3, surname3, Id3);
+                            if (string.Equals(name3, client3.name))
+                            {
+                                car3 = adminCar.GetCar(mark3, model3, client3.idClient);
+                                entryFound = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        car3 = adminCar.GetCar(mark3, model3, Id3);
+                        if (string.Equals(model3, car3.model))
+                        {
+                            client3 = adminClient.GetClient(name3, surname3, Id3);
+                            entryFound = true;
+                        }
+                    }
+                }
+                else
+                {
+                    car3 = adminCar.GetCar(mark3, model3, Id3);
+                    if (string.Equals(mark3, car3.mark))
+                    {
+                        client3 = adminClient.GetClient(name3, surname3, Id3);
+                        entryFound = true;
+                    }
+                }
+            }
+            else
+            {
+                car3 = adminCar.GetCar(mark3, model3, Id3);
+                if (Id3 == car3.idCar)
+                {
+                    client3 = adminClient.GetClient(name3, surname3, Id3);
+                    entryFound = true;
+                }
+            }
+
+            if (entryFound == true)
+            {
+                string[] odostr = Array.ConvertAll<int, string>(car3.lastodometer, ele => ele.ToString());
+                string customerInfo = string.Format("Client with Id #{0} is: {1} {2} and has the car {3} {4} and has had checks at {5}",
+                    client3.idClient,
+                    client3.name ?? " N/A ",
+                    client3.surname ?? " N/A ",
+                    car3.mark ?? " N/A ",
+                    car3.model ?? " N/A ",
+                    (string.Join(" ", odostr) ?? " N/A "));
+
+                label6.BackColor = Color.PaleGreen;
+                label8.BackColor = Color.PaleGreen;
+                label7.BackColor = Color.PaleGreen;
+                label9.BackColor = Color.PaleGreen;
+            }
         }
     }
 }
